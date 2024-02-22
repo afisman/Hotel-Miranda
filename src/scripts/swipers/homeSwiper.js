1e4955e8b8394b762ce27d0b683aecd9f86d1b90
afisman <afisman@gmail.com>
2024-02-22 11:31:52 +0100

    feat: finish features home slider

--- src/scripts/swipers/homeSwiper.js
+++ src/scripts/swipers/homeSwiper.js
@@ -6,11 +6,31 @@ const swiper = new Swiper('.roomSection__swiper', {
         prevEl: ".swiper-button-prev",
         nextEl: ".swiper-button-next",
     },
-    // breakpoints: {
-    //     1000: {
-    //         slidesPerView: 3,
-    //     },
-    // },
     mousewheel: true,
     keyboard: true,
 });
+
+const featuresSwiper = new Swiper('.featuresSection__swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+document.onscroll = () => {
+    const width = document.body.offsetWidth;
+    if (width > 1000) {
+        featuresSwiper.params.slidesPerView = 3
+        featuresSwiper.params.grid.rows = 2
+        featuresSwiper.params.grid.fill = "row"
+        featuresSwiper.params.centeredSlides = false
+        featuresSwiper.update()
+
+
+    }
+}
